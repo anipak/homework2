60050b3009ab782a6f7599fa711d9a7556551373
anipak <127544078+anipak@users.noreply.github.com>
2023-04-14 12:01:05 +0300

    ДЗ 3 сделала

---
+++ DZ3_23-05/DZ3/app.js
@@ -0,0 +1,24 @@
+// Задание 1
+
+// for (var i = 1; i < 100; i++){
+//     if (i % 3 ===0 && i % 5 === 0){
+//         console.log('FizzBuss');
+//     }else if (i % 3 ===0  ){
+//         console.log('Fizz');
+//     }else if (i % 5 === 0){
+//         console.log('Buzz')
+//     }
+// }
+
+
+
+// Задание 2
+//
+var drinks = ['milk', 'beer', 'beer', 'milk', 'milk']
+for (var i = 0; i < drinks.length; i++){
+    if (drinks [i] === 'milk'){
+        console.log(`${drinks [i]} good`)
+    }else if (drinks [i] === 'beer')
+    console.log(`${drinks [i]} bad`)
+}
+
